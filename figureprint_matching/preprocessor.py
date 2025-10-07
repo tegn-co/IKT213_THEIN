c5f582828c958edbec0b4edc607ab5dcbb41f93c
Saw John Thein <sawjt@student.uia.no>
2025-10-07 18:19:57 +0200

    changes to preprocessor

--- figureprint_matching/preprocessor.py
+++ figureprint_matching/preprocessor.py
@@ -6,7 +6,7 @@ def preprocess_figureprint(image_path):
     #cv.imshow("original",img)
 
     if img is None:
-        raise FileNotFoundError(f"Kunne ikke lese bilde: {image_path}")
+        raise FileNotFoundError(f"Could not read the image: {image_path}")
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     #cv.imshow("gray",img)
     _, img_bin = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
